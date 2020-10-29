cffbf59e95043b2bb74890c5a8adfcb2f69088a5
Tim Ramandt <tim.ramandt@hotmail.com>
2020-10-29 19:12:51 +0100

    simplified version is ready

--- webclient/scripts/datascript.js
+++ webclient/scripts/datascript.js
@@ -1,11 +1,25 @@
-readData();
+readData()
 
 async function readData() {
-    const response = await fetch("http://localhost:8088/data/s4/data.txt");
-    const data = await response.text();
-    return parseData(data); 
-    //var fileReader = new FileReader();
-    //return fileReader.readAsText("./data/s4/data.txt")
+    const response = await fetch("http://localhost:8088/data/s4/data.txt")
+    const data = await response.text()
+    parseData(data)
+}
+
+function parseData(data) {
+    console.log(data)
+    var lines = data.split("\n")
+
+    for (lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        if (lines[lineIndex] === "") {
+            continue;
+        }
+
+        var split = lines[lineIndex].split(":");
+
+        var outcome = split[0];
+        visualizeData(outcome, pokemonsToArray(split[1]))
+    }
 }
 
 function visualizeData(outcome, pokemons) {
@@ -14,47 +28,43 @@ function visualizeData(outcome, pokemons) {
 
     var clazz = document.createAttribute("class");
     clazz.value = outcome;
-    row.setAttributeNode(clazz);
+    row.setAttributeNode(clazz)
 
-    for (i = 0; i < pokemons.length; i++) {
-        console.log(pokemons[i])
+    console.log(pokemons)
+    for (pokemonIndex = 0; pokemonIndex < pokemons.length; pokemonIndex++) {
         var tabledata = document.createElement("td");
-        var textNode = document.createTextNode(pokemons[i]);
+        var textNode = document.createTextNode(pokemons[pokemonIndex]);
         tabledata.append(textNode)
 
         row.append(tabledata);
     }
 
     table.append(row);
+    console.log("appended!")
 }
 
-function parseData(data) {
-    var split = data.split(":");
-
-    var outcome = split[0];
-    var pokemonsData = split[1];
-
-    var pokemons = new Array();
+function pokemonsToArray(pokemons) {
+    console.log(pokemons)
+    var pokemonsArray = new Array();
     var pokemon = ""; 
-
-    var charArray = Array.from(pokemonsData)
-    for (i = 0; i < charArray.length; i++) {
-       if (charArray[i] == ",") {
-           pokemons.push(pokemon)
+    
+    var charArray = Array.from(pokemons)
+    for (charIndex = 0; charIndex < charArray.length; charIndex++) {
+        if (charArray[charIndex] == ",") {
+           pokemonsArray.push(pokemon)
            pokemon = "";
            continue;
        }
 
-        if (isAlpha(charArray[i])) {
-            pokemon += charArray[i];
+        if (isAlpha(charArray[charIndex])) {
+            pokemon += charArray[charIndex];
         }
     }
 
-    pokemons.push(pokemon);
-
-    visualizeData(outcome, pokemons) 
+    pokemonsArray.push(pokemon)
+    return pokemonsArray
 }
 
 function isAlpha(char) {
-    return /^[A-Z]$/i.test(char);
+    return /^[A-Z]$/i.test(char);;
 }
