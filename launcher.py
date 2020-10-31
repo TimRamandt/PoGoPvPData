38689fd2f9c59b3435723e7ffb906521005f9885
Tim Ramandt <tim.ramandt@hotmail.com>
2020-10-31 04:03:18 +0100

    implemented the daily limit of 30 for the launcher

--- launcher.py
+++ launcher.py
@@ -1,12 +1,49 @@
+import datetime
+from colorama import Fore 
+
 def launch():
     print("Pokemon Go PvP Data Collector.")
+
+    amountOfbattles = determineRemainingBattles()
+    if amountOfbattles >= 30:
+        errorMessage("Unable to input more battles, max amount (6) of sets is reached for today") 
+        return
+
     print("Syntax")
     print("W/L/D:Pokemon1,Pokemon2,Pokemon3")
-    userInput = input("> ")
 
-    writeToFile(userInput)
+    #battle registration
+    while amountOfbattles < 30:
+        userInput = input("> ")
+        writeToFile(userInput)
+        amountOfbattles += 1
 
+    errorMessage("Unable to input more battles, max amount (6) of sets is reached for today") 
     #parseData(userInput)
+
+def determineRemainingBattles():
+    content = open("webclient/data/s4/data.txt", "r").read().splitlines()
+    lineIndex = len(content) - 1
+    amountOfBattles = 0
+    while lineIndex >= 0:
+        if content[lineIndex].startswith('- '):
+            if isInPast(content[lineIndex].split(' ')[2]):
+                print("Welcome back trainer, good luck on today's battles.")
+                writeToFile("- GL " + datetime.date.today().strftime("%Y-%m-%d"))
+                return 0
+            
+            print("You have do " + str(amountOfBattles) + " battles so far.")
+            return amountOfBattles
+
+        amountOfBattles += 1
+        lineIndex -= 1
+
+    return 0
+
+def isInPast(strDate):
+    now = datetime.date.today()
+    dateArray = strDate.split('-')
+    return datetime.datetime(now.year, now.month, now.day) > datetime.datetime(int(dateArray[0]), int(dateArray[1]), int(dateArray[2]))
 
 
 def parseData(userInput):
@@ -27,9 +64,16 @@ def parseData(userInput):
     pokemons.append(pokemon)
     print(pokemons)
 
-
 def writeToFile(input):
     file = open("webclient/data/s4/data.txt", "a")
     file.write(input + "\n")
+
+
+def errorMessage(errorMessage):
+    print(Fore.RED + errorMessage)
+
+    #reset the console back to the regular color
+    print(Fore.RESET)
+
 
 launch()
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ This application uses Python. You'll need atleast Python 3.0 because we spin a l
 # How to use
 
 ## collect/insert data
-Oo collect data you'll have to run a console script.
+collect data you'll have to run a console script.
 
 1. open a terminal and go to this root folder in your terminal
 2. type ```python launcher.py```
@@ -16,7 +16,7 @@ Oo collect data you'll have to run a console script.
 ## view data
 to view your data, go to 
 
-1. ```webclient``` folder and run ```pyhton server.py```
+1. ```webclient``` folder and run ```python server.py```
 2. open your brower and type ```localhost:8088```
 
 You'll be able to see your data.
